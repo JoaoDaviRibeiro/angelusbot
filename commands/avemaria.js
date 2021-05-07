16eb6f3d6dcd2bb422583041de47c50442797ca6
João Davi Ribeiro <81536521+JoaoDaviRibeiro@users.noreply.github.com>
2021-05-07 18:30:54 -0300

    Update avemaria.js

--- commands/avemaria.js
+++ commands/avemaria.js
@@ -2,7 +2,14 @@ module.exports = {
     name: 'avemaria',
     description: "this is a ave maria command!",
     execute(message, args){
-        message.channel.send('Ave Maria, cheia de graça, o Senhor é convosco. Bendita sois vós entre as mulheres, e bendito é o fruto do vosso ventre, Jesus. Santa Maria mãe de Deus, rogai por nós pecadores. Agora e na hora de nossa morte, amém. ');
+       if(message.member.roles.cache.has('824400698896285747')){
+          message.channel.send('```Ave Maria, cheia de graça, o Senhor é convosco. Bendita sois vós entre as mulheres, e bendito é o fruto do vosso ventre, Jesus. Santa Maria mãe de Deus, rogai por nós pecadores. Agora e na hora de nossa morte, amém.``` ');
+       
+       
+    }else {
+        message.channel.send('Converta-se ao catolicismo.');
+    }
+        
         
     }
 }
